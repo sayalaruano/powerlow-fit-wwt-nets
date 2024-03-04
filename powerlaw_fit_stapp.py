88c8b6ecbe15dc404fdc63b4769576891d2919af
sayalaruano <sebasar1245@gmail.com>
2024-03-04 13:28:55 +0100

    Create plots for xmin as min degree and xmin as optimal value

--- powerlaw_fit_stapp.py
+++ powerlaw_fit_stapp.py
@@ -45,7 +45,7 @@ def plot_power_law_plotly(df):
     
     return fig
 
-def plot_power_law_matplotlib(df, compare_distribution):
+def plot_power_law_matplotlib(df, compare_distribution, xmin = None):
     # Fit the data to a power law distribution
     data = df['Degree'].values
     data = data[data > 0]
@@ -54,7 +54,7 @@ def plot_power_law_matplotlib(df, compare_distribution):
     if len(data) == 0:
         raise ValueError("No positive data available to plot after filtering.")
     
-    fit = powerlaw.Fit(data)
+    fit = powerlaw.Fit(data, xmin=xmin, discrete=False)
 
     # Create a figure with a specific size
     fig, ax = plt.subplots(figsize=(7, 3.5))  # Adjust figure size if needed
@@ -78,7 +78,7 @@ def plot_power_law_matplotlib(df, compare_distribution):
 
     return fig
 
-def compare_distributions(df, distribution1, distribution2):
+def compare_distributions(df, distribution1, distribution2, xmin = None):
     # Filter data for positive values
     data = df['Degree'].values
     data = data[data > 0]
@@ -86,7 +86,7 @@ def compare_distributions(df, distribution1, distribution2):
     if len(data) == 0:
         return None, None  # Return None if no positive data
 
-    fit = powerlaw.Fit(data)
+    fit = powerlaw.Fit(data, xmin=xmin, discrete=False)
     R, p = fit.distribution_compare(distribution1, distribution2, normalized_ratio=True)
     return R, p
 
@@ -102,8 +102,6 @@ biome = st.selectbox('Select the biome to analyze', biomes)
 available_distributions = ['lognormal', 'exponential', 'truncated_power_law', 'stretched_exponential', 'lognormal_positive']
 selected_distribution = st.selectbox('Select a distribution to compare with power law fit', available_distributions)
 
-st.write("Select the threshold and the complementary cumulative distribution function (CCDF) plots will be displayed.")
-
 # Define file paths
 file_paths = [
     f'Data/{biome_folder_names[biomes.index(biome)]}/{biome_folder_names[biomes.index(biome)]}_cclasso_010_degree_distribution.csv',
@@ -115,12 +113,43 @@ file_paths = [
     f'Data/{biome_folder_names[biomes.index(biome)]}/{biome_folder_names[biomes.index(biome)]}_cclasso_050_degree_distribution.csv'
 ]
 
+st.subheader('Complementary Cumulative Distribution Function (CCDF) plots with xmin as the minimum degree')
+st.write("Select the threshold and the complementary cumulative distribution function (CCDF) plots will be displayed.")
+
 # List of thresholds
 thresholds = [0.10, 0.20, 0.30, 0.40, 0.45, 0.47, 0.50]
 
 # Create tabs for each plot adding the threshold to the tab name
 tab_names = [f"CCLasso_ {threshold}" for threshold in thresholds]
-# tab_names = [f"Plot {idx+1}" for idx, _ in enumerate(file_paths)]
+tabs = st.tabs(tab_names)
+
+for idx, file_path in enumerate(file_paths):
+    with tabs[idx]:
+        if os.path.exists(file_path):
+            df = load_data(file_path)
+            min_degree = df['Degree'].min()
+            try:
+                fig = plot_power_law_matplotlib(df, selected_distribution, xmin = min_degree)
+                st.pyplot(fig, use_container_width=False)
+
+                # Perform and display the distribution comparison
+                R, p = compare_distributions(df, 'power_law', selected_distribution, xmin = min_degree)
+                if R is not None and p is not None:
+                    st.write(f"Loglikelihood ratio between Power Law and {selected_distribution.capitalize()}: {R:.2f}")
+                    st.write(f"Significance of the comparison (p-value): {p:.2f}")
+                else:
+                    st.write("Not enough positive data for distribution comparison.")
+                
+            except ValueError as e:
+                st.error(str(e))
+        else:
+            st.error(f"Data file not found for {tab_names[idx]}. Please check the file path or the data availability.")
+
+st.subheader('Complementary Cumulative Distribution Function (CCDF) plots with optimal xmin')
+st.write("Select the threshold and the complementary cumulative distribution function (CCDF) plots will be displayed.")
+
+# Create tabs for each plot adding the threshold to the tab name
+tab_names = [f"CCLasso_ {threshold}" for threshold in thresholds]
 tabs = st.tabs(tab_names)
 
 for idx, file_path in enumerate(file_paths):
@@ -130,8 +159,6 @@ for idx, file_path in enumerate(file_paths):
             try:
                 fig = plot_power_law_matplotlib(df, selected_distribution)
                 st.pyplot(fig, use_container_width=False)
-                # fig_html = mpld3.fig_to_html(fig)
-                # components.html(fig_html, height=500, width=900)
 
                 # Perform and display the distribution comparison
                 R, p = compare_distributions(df, 'power_law', selected_distribution)
